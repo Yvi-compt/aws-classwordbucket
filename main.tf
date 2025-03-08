cc6f389d3791e823bd935361c5953d33755c7d0d
Yves PLACIDE <yvesplacide@gmail.com>
2025-03-07 19:22:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Create 5 S3 buckets
 resource "aws_s3_bucket" "yvibuckets" {
   count  = 5
-  bucket = "var.bucket_name-${count.index + 1}-12345" # Replace with a unique prefix
+  bucket = "yvibucket-${count.index + 1}-12345" # Replace with a unique prefix
  # acl    = "private"
 
   tags = {
