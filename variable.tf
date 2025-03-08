a7a8892b037a95a6e2f77b2e07e275737dcc75c3
Yves PLACIDE <yvesplacide@gmail.com>
2025-03-07 19:00:24 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,19 @@
+/*variable "bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket"
+}*/
+
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  sensitive   = true
+}
